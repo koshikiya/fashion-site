12a7e78ff8851c96d6a27fbad8e8df35b16983fd
koshikiya <rhxck.lv@gmail.com>
2019-12-16 10:27:41 +0000

    resize

--- resources/views/fashions/index.blade.php
+++ resources/views/fashions/index.blade.php
@@ -9,7 +9,21 @@
         @foreach ($fashions as $fashion)
                 <dl>
                     <dd><a href="{{ action('FashionsController@show', $fashion->id) }}"><img src="/storage/image/{{$fashion->photo}}" width="230" height="300"></a></dd>
-                    <dd>{{ $fashion->user->name }}</dd>
+                    <dd>
+                        {{ $fashion->user->name }}
+                        @if (Auth::id() != $fashion->user->id)
+                            @if (Auth::user()->following($fashion->user->id))
+                                {!! Form::open(['route' => ['user.unfollow', $fashion->user->id], 'method' => 'delete']) !!}
+                                    {!! Form::submit('フォロー外す', ['class' => "btn btn-default btn-sm"]) !!}
+                                {!! Form::close() !!}
+                            @else
+                                {!! Form::open(['route' => ['user.follow', $fashion->user->id]]) !!}
+                                    {!! Form::submit('フォローする', ['class' => "btn btn-default btn-sm"]) !!}
+                                {!! Form::close() !!}
+                            @endif
+                        @endif   
+                    </dd>
+                    
                 </dl>
         @endforeach
     </div>
--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -12,16 +12,16 @@
             <td>{{ $user->id }}</td>
     
     @if (Auth::id() != $user->id)
-    @if (Auth::user()->following($user->id))
-        {!! Form::open(['route' => ['user.unfollow', $user->id], 'method' => 'delete']) !!}
-            {!! Form::submit('フォロー外す', ['class' => "btn btn-default btn-sm"]) !!}
-        {!! Form::close() !!}
-    @else
-        {!! Form::open(['route' => ['user.follow', $user->id]]) !!}
-            {!! Form::submit('フォローする', ['class' => "btn btn-default btn-sm"]) !!}
-        {!! Form::close() !!}
-    @endif
-@endif   
+        @if (Auth::user()->following($user->id))
+            {!! Form::open(['route' => ['user.unfollow', $user->id], 'method' => 'delete']) !!}
+                {!! Form::submit('フォロー外す', ['class' => "btn btn-default btn-sm"]) !!}
+            {!! Form::close() !!}
+        @else
+            {!! Form::open(['route' => ['user.follow', $user->id]]) !!}
+                {!! Form::submit('フォローする', ['class' => "btn btn-default btn-sm"]) !!}
+            {!! Form::close() !!}
+        @endif
+    @endif   
 
 
 @endsection
