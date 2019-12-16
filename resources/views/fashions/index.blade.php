f3005e2644ead05ac3f48503eaeebc59c8621fd6
koshikiya <rhxck.lv@gmail.com>
2019-12-16 10:38:24 +0000

    add photo

--- resources/views/fashions/index.blade.php
+++ resources/views/fashions/index.blade.php
@@ -9,20 +9,9 @@
         @foreach ($fashions as $fashion)
                 <dl>
                     <dd><a href="{{ action('FashionsController@show', $fashion->id) }}"><img src="/storage/image/{{$fashion->photo}}" width="230" height="300"></a></dd>
-                    <dd>
+                    <div class="user">
                         {{ $fashion->user->name }}
-                        @if (Auth::id() != $fashion->user->id)
-                            @if (Auth::user()->following($fashion->user->id))
-                                {!! Form::open(['route' => ['user.unfollow', $fashion->user->id], 'method' => 'delete']) !!}
-                                    {!! Form::submit('フォロー外す', ['class' => "btn btn-default btn-sm"]) !!}
-                                {!! Form::close() !!}
-                            @else
-                                {!! Form::open(['route' => ['user.follow', $fashion->user->id]]) !!}
-                                    {!! Form::submit('フォローする', ['class' => "btn btn-default btn-sm"]) !!}
-                                {!! Form::close() !!}
-                            @endif
-                        @endif   
-                    </dd>
+                    </div>
                     
                 </dl>
         @endforeach
